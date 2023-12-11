a08f0ce4081ca52cb11b309882c23d6475f25949
jjimgua966 <jjimgua966@educaand.es>
2023-12-11 12:00:07 +0100

    Version 3

--- app/src/main/java/com/example/prueba_t2/ResultIMCActivity.kt
+++ app/src/main/java/com/example/prueba_t2/ResultIMCActivity.kt
@@ -8,7 +8,13 @@ import androidx.activity.OnBackPressedCallback
 
 class ResultIMCActivity : AppCompatActivity() {
 
+    private lateinit var tvSexo: TextView
+    private lateinit var tvEdad: TextView
+    private lateinit var tvAltura: TextView
+    private lateinit var tvPeso: TextView
+    private lateinit var tvTipoPeso: TextView
     private lateinit var tvIMC: TextView
+    private lateinit var tvMensaje: TextView
     private lateinit var butRecalcular: Button
     private var sexo: Int = 0
     private var altura: Int = 0
@@ -24,7 +30,13 @@ class ResultIMCActivity : AppCompatActivity() {
     }
 
     private fun setVariables() {
+        tvSexo = findViewById(R.id.tvSexo)
+        tvEdad = findViewById(R.id.tvEdad)
+        tvAltura = findViewById(R.id.tvAltura)
+        tvPeso = findViewById(R.id.tvPeso)
+        tvTipoPeso = findViewById(R.id.tvTipoPeso)
         tvIMC = findViewById(R.id.tvIMC)
+        tvMensaje = findViewById(R.id.tvMensaje)
         butRecalcular = findViewById(R.id.butRecalcular)
         sexo = intent.extras?.getInt("EXTRA_SEXO") ?: 0
         altura = intent.extras?.getInt("EXTRA_ALTURA") ?: 0
@@ -34,8 +46,52 @@ class ResultIMCActivity : AppCompatActivity() {
     }
 
     private fun setActions() {
-        val resultado = calcularIMC()
-        tvIMC.text = String.format("%.2f", resultado)
+        val IMC = calcularIMC()
+        tvIMC.text = String.format("%.2f", IMC)
+
+        when (sexo) {
+            Constantes.valMujer -> tvSexo.text = String.format("Mujer")
+            else -> tvSexo.text = String.format("Hombre")
+        }
+
+        tvEdad.text = String.format("$edad años")
+        tvAltura.text = String.format("$altura cm")
+        tvPeso.text = String.format("$peso kg")
+
+        when (IMC) {
+            in 0.0..18.5 -> {
+                tvTipoPeso.text = String.format("Peso Bajo")
+                if (sexo == Constantes.valMujer) {
+                    tvMensaje.text = String.format("Estás muy delgada \n Tienes que ganar peso")
+                } else {
+                    tvMensaje.text = String.format("Estás muy delgado \n Tienes que ganar peso")
+                }
+            }
+            in 18.6..25.0 -> {
+                tvTipoPeso.text = String.format("Normal")
+                if (sexo == Constantes.valMujer) {
+                    tvMensaje.text = String.format("Estás muy bien, tía!!!! \n Sigue así!!!!")
+                } else {
+                    tvMensaje.text = String.format("Estás muy bien, tío!!!! \n Sigue así!!!!")
+                }
+            }
+            in 26.0..30.0 -> {
+                tvTipoPeso.text = String.format("Sobrepeso")
+                if (sexo == Constantes.valMujer) {
+                    tvMensaje.text = String.format("Estás rellenita \n Cuídate!!!")
+                } else {
+                    tvMensaje.text = String.format("Estás rellenito \n Cuídate!!!")
+                }
+            }
+            else -> {
+                tvTipoPeso.text = String.format("Obesidad")
+                if (sexo == Constantes.valMujer) {
+                    tvMensaje.text = String.format("Estás gordito \n Tienes que adelgazar!!!")
+                } else {
+                    tvMensaje.text = String.format("Estás gordito \n Tienes que adelgazar!!!")
+                }
+            }
+        }
 
         butRecalcular.setOnClickListener {
             super.onBackPressed()
